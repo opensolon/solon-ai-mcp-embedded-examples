e007ae8f063f351960b6ae1122323694e5c2932d
noear <noear@live.cn>
2025-04-26 12:16:49 +0800

    demo

--- solon-ai-mcp-embedded-vertx/src/main/java/webapp/mcpserver/McpServerConfig.java
+++ solon-ai-mcp-embedded-vertx/src/main/java/webapp/mcpserver/McpServerConfig.java
@@ -3,7 +3,10 @@ package webapp.mcpserver;
 import io.vertx.core.AbstractVerticle;
 import io.vertx.ext.web.Router;
 import org.noear.solon.Solon;
+import org.noear.solon.ai.chat.tool.MethodToolProvider;
+import org.noear.solon.ai.mcp.server.McpServerEndpointProvider;
 import org.noear.solon.web.vertx.VxWebHandler;
+import webapp.mcpserver.tool.McpServerTool2;
 
 /**
  * 这个类独立一个目录，可以让 Solon 扫描范围最小化
@@ -24,6 +27,13 @@ public class McpServerConfig extends AbstractVerticle {
         });
 
         Solon.start(McpServerConfig.class, new String[]{"--cfg=mcpserver.yml"});
+
+        //手动构建 mcp 服务端点
+        McpServerEndpointProvider serverEndpointProvider = McpServerEndpointProvider.builder()
+                .sseEndpoint("/mcp/demo2/sse")
+                .build();
+        serverEndpointProvider.addTool(new MethodToolProvider(new McpServerTool2()));
+        serverEndpointProvider.postStart();
     }
 
     @Override
--- solon-ai-mcp-embedded-vertx_port/src/main/java/webapp/mcpserver/McpServerConfig.java
+++ solon-ai-mcp-embedded-vertx_port/src/main/java/webapp/mcpserver/McpServerConfig.java
@@ -2,6 +2,9 @@ package webapp.mcpserver;
 
 import io.vertx.core.AbstractVerticle;
 import org.noear.solon.Solon;
+import org.noear.solon.ai.chat.tool.MethodToolProvider;
+import org.noear.solon.ai.mcp.server.McpServerEndpointProvider;
+import webapp.mcpserver.tool.McpServerTool2;
 
 /**
  * 这个类独立一个目录，可以让 Solon 扫描范围最小化
@@ -11,6 +14,13 @@ public class McpServerConfig extends AbstractVerticle {
     @Override
     public void start() {
         Solon.start(McpServerConfig.class, new String[]{"--cfg=mcpserver.yml"});
+
+        //手动构建 mcp 服务端点
+        McpServerEndpointProvider serverEndpointProvider = McpServerEndpointProvider.builder()
+                .sseEndpoint("/mcp/demo2/sse")
+                .build();
+        serverEndpointProvider.addTool(new MethodToolProvider(new McpServerTool2()));
+        serverEndpointProvider.postStart();
     }
 
 
