f3bcd24becccd073da19e68f3f22a28cbc5b5d68
noear <noear@live.cn>
2025-04-26 12:07:14 +0800

    demo

--- solon-ai-mcp-embedded-solon/src/main/java/webapp/HelloApp.java
+++ solon-ai-mcp-embedded-solon/src/main/java/webapp/HelloApp.java
@@ -1,26 +1,24 @@
 package webapp;
 
 import org.noear.solon.Solon;
+import org.noear.solon.ai.chat.tool.MethodToolProvider;
+import org.noear.solon.ai.mcp.server.McpServerEndpointProvider;
 import org.noear.solon.annotation.Controller;
 import org.noear.solon.annotation.Get;
 import org.noear.solon.annotation.Mapping;
+import webapp.mcpserver.tool.McpServerTool2;
 
 @Controller
 public class HelloApp {
     public static void main(String[] args) {
-        Solon.start(HelloApp.class, args);
-    }
+        Solon.start(HelloApp.class, args, app -> {
+            //手动构建 mcp 服务端点
+            McpServerEndpointProvider serverEndpointProvider = McpServerEndpointProvider.builder()
+                    .sseEndpoint("/mcp/demo2/sse")
+                    .build();
+            serverEndpointProvider.addTool(new MethodToolProvider(new McpServerTool2()));
 
-    @Get
-    @Mapping("/")
-    public String hello(String name){
-        return "hello world: " + name;
-    }
-
-    @Get
-    @Mapping("/hello2")
-    public String hello2(String name) throws Exception{
-        Thread.sleep(10);
-        return "hello world: " + name;
+            app.context().lifecycle(serverEndpointProvider);
+        });
     }
 }
