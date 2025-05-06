5c54a2091d29e0f7b17e290f9ebbea0b18a2ee05
noear <noear@live.cn>
2025-05-06 16:17:38 +0800

    demo

--- solon-ai-mcp-embedded-solon/src/main/java/webapp/mcpserver/tool/McpServerTool.java
+++ solon-ai-mcp-embedded-solon/src/main/java/webapp/mcpserver/tool/McpServerTool.java
@@ -1,8 +1,8 @@
 package webapp.mcpserver.tool;
 
-import org.noear.solon.ai.chat.annotation.ToolMapping;
-import org.noear.solon.ai.chat.annotation.ToolParam;
+import org.noear.solon.ai.annotation.ToolMapping;
 import org.noear.solon.ai.mcp.server.annotation.McpServerEndpoint;
+import org.noear.solon.annotation.Param;
 
 /**
  * 自动构建服务端点服务
@@ -13,7 +13,7 @@ public class McpServerTool {
     // 建议开启编译参数：-parameters （否则，最好再配置参数的 name）
     //
     @ToolMapping(description = "查询天气预报")
-    public String getWeather(@ToolParam(description = "城市位置") String location) {
+    public String getWeather(@Param(description = "城市位置") String location) {
         return "晴，14度";
     }
 }
--- solon-ai-mcp-embedded-springboot3/src/main/java/webapp/mcpserver/tool/McpServerTool2.java
+++ solon-ai-mcp-embedded-springboot3/src/main/java/webapp/mcpserver/tool/McpServerTool2.java
@@ -1,7 +1,7 @@
 package webapp.mcpserver.tool;
 
-import org.noear.solon.ai.chat.annotation.ToolMapping;
-import org.noear.solon.ai.chat.annotation.ToolParam;
+import org.noear.solon.ai.annotation.ToolMapping;
+import org.noear.solon.annotation.Param;
 
 /**
  * 用于手动构建
@@ -11,7 +11,7 @@ public class McpServerTool2 {
     // 建议开启编译参数：-parameters （否则，最好再配置参数的 name）
     //
     @ToolMapping(description = "查询天气预报")
-    public String getWeather(@ToolParam(description = "城市位置") String location) {
+    public String getWeather(@Param(description = "城市位置") String location) {
         return "晴，14度";
     }
 }
