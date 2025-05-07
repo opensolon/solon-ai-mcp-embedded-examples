fe51dab3b3476ad5061be0d8ce5e5337c739bed3
noear <noear@live.cn>
2025-05-07 17:30:48 +0800

    demo

--- solon-ai-mcp-embedded-springboot2_port/src/main/java/webapp/HelloApp.java
+++ solon-ai-mcp-embedded-springboot2_port/src/main/java/webapp/HelloApp.java
@@ -14,6 +14,13 @@ public class HelloApp {
 
     @RequestMapping("/")
     public String hello(String name){
+//        //动态获取（添加工具等）//只是示例，可以删掉
+//        McpServerEndpointProvider serverEndpointProvider = Solon.context().getBean("demo1");
+//
+//        serverEndpointProvider.addTool(new FunctionToolDesc(null));
+//        serverEndpointProvider.addResource(new FunctionResourceDesc(null));
+//        serverEndpointProvider.addPrompt(new FunctionPromptDesc(null));
+
         return "hello world: " + name;
     }
 
