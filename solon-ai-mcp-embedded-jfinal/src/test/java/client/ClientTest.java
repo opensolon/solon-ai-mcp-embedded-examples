7b74d0ae48cc4f6d8304bef8465e598b6e666382
noear <noear@live.cn>
2025-05-08 09:03:54 +0800

    demo

--- solon-ai-mcp-embedded-jfinal/src/test/java/client/ClientTest.java
+++ solon-ai-mcp-embedded-jfinal/src/test/java/client/ClientTest.java
@@ -2,6 +2,7 @@ package client;
 
 import org.junit.jupiter.api.Test;
 import org.noear.solon.ai.chat.ChatModel;
+import org.noear.solon.ai.chat.ChatResponse;
 import org.noear.solon.ai.chat.message.ChatMessage;
 import org.noear.solon.ai.mcp.client.McpClientProvider;
 import org.noear.solon.test.SolonTest;
@@ -14,6 +15,9 @@ import java.util.Map;
 
 @SolonTest(HelloApp.class)
 public class ClientTest {
+    /**
+     * 工具直接调用
+     */
     @Test
     public void case1() throws Exception {
         McpClientProvider toolProvider = McpClientProvider.builder()
@@ -36,7 +40,8 @@ public class ClientTest {
 
         System.out.println("---------------");
 
-        /// //////////////////////
+        /// /////////////////
+
 
         toolProvider = McpClientProvider.builder()
                 .apiUrl("http://localhost:8080/mcp/demo2/sse")
@@ -57,12 +62,29 @@ public class ClientTest {
         System.out.println(resourceContent);
     }
 
-    public void demo(McpClientProvider toolProvider) throws Exception {
-        ChatModel chatModel = ChatModel.of("...")
+    //换成自己的模型配置（参考：https://solon.noear.org/article/918）
+    private static final String apiUrl = "http://127.0.0.1:11434/api/chat";
+    private static final String provider = "ollama";
+    private static final String model = "qwen2.5:1.5b"; //"llama3.2";//deepseek-r1:1.5b;
+
+    /**
+     * 与大模型集成
+     */
+    @Test
+    public void case2() throws Exception {
+        McpClientProvider toolProvider = McpClientProvider.builder()
+                .apiUrl("http://localhost:8080/mcp/demo1/sse")
+                .build();
+
+        ChatModel chatModel = ChatModel.of(apiUrl)
+                .provider(provider)
+                .model(model)
                 .defaultToolsAdd(toolProvider) //添加默认工具
                 .build();
 
-        chatModel.prompt("杭州今天的天气怎么样？")
+        ChatResponse resp = chatModel.prompt("杭州今天的天气怎么样？")
                 .call();
+
+        System.out.println(resp.getMessage());
     }
 }
