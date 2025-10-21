8d369fe1990d44386c654a53965c5a5a07ac2c61
noear <noear@live.cn>
2025-10-21 10:51:27 +0800

    demo

--- solon-ai-in-springmvc/src/main/java/webapp/HelloApp.java
+++ solon-ai-in-springmvc/src/main/java/webapp/HelloApp.java
@@ -1,12 +1,32 @@
 package webapp;
 
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
 /**
  *
  * @author noear 2025/10/21 created
  *
  */
-public class HelloApp {
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = "webapp")
+public class HelloApp implements WebMvcConfigurer {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+
+    }
+
+    @Override
+    public void configureViewResolvers(ViewResolverRegistry registry) {
+
     }
 }
